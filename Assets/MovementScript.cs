872aced5ace9cff53729aaa066c7a425d8597c21
chymishhh <vandarkslave@outlook.com>
2022-12-22 22:52:07 +0300

    Renamed some files

--- Assets/MovementScript.cs
+++ Assets/MovementScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovmentScript : MonoBehaviour
+public class MovementScript : MonoBehaviour
 {
     public CharacterController Controller;
 
