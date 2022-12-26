fe6c571509635e11474bd7a6806a9edac1887736
AcoStar <greenshiftkaito@hotmail.com>
2022-12-26 22:13:02 +0300

    Drop item function

--- Assets/Scripts/RaccoonAI.cs
+++ Assets/Scripts/RaccoonAI.cs
@@ -44,12 +44,37 @@ public class RaccoonAI : MonoBehaviour
         //  Украденный предмет больше не взаимодействует с миром
         target.GetComponent<InterestingObject>().Locked = true;
         Rigidbody targetRB = target.GetComponent<Rigidbody>();
-        targetRB.detectCollisions = false;
-        targetRB.useGravity = false;
-        targetRB.isKinematic = true;
+        if (targetRB != null)
+        {
+            targetRB.detectCollisions = false;
+            targetRB.useGravity = false;
+            targetRB.isKinematic = true;
+        }
 
         //  Перемещение предмета на место рук
         target.transform.SetParent(transform);
         target.transform.position = handTransform.position;
+    }
+
+    /// <summary>
+    /// Выбросить предмет из рук
+    /// </summary>
+    public void Drop()
+    {
+        if (itemInHands == null)
+            return;
+
+        Rigidbody targetRB = itemInHands.GetComponent<Rigidbody>();
+        if (targetRB != null)
+        {
+            targetRB.detectCollisions = true;
+            targetRB.useGravity = true;
+            targetRB.isKinematic = false;
+        }
+
+        itemInHands.transform.SetParent(null);
+        itemInHands.GetComponent<InterestingObject>().Locked = false;
+        itemInHands = null;
+        AnimalAI.InterestedInTarget = true;
     }
 }
