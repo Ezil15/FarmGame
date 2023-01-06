98e7403e44e36a9d2526cd42b581d8d9bb23c42d
chymishhh <vandarkslave@outlook.com>
2023-01-06 21:04:49 +0300

    new event, changed disappear time

--- Assets/Scripts/ResourceComponent.cs
+++ Assets/Scripts/ResourceComponent.cs
@@ -1,27 +1,39 @@
 using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class OnDisappearedEvent : UnityEvent<GameObject> {}
 
 public class ResourceComponent : MonoBehaviour
 {
     public GameObject Object;
+    public OnDisappearedEvent OnDisappearedEvent;
     public int Price;
+    public int Size;
     public bool Despawnable = true;
-    public bool isTaken = false;
-    public int size;
+    private bool isTaken = false;
     [SerializeField]
     private float disappearTickCooldown;
+    private float currentTick = 0f;
     
 
     private void FixedUpdate() 
     {
-        if(isTaken)
-            return;
         if(!Despawnable)
             return;
-        disappearTickCooldown -= Time.deltaTime;
+        if(isTaken)
+        {
+            if(disappearTickCooldown - currentTick <= 1f)
+                currentTick = 1.5f;
+            return;
+        }
+        currentTick += Time.deltaTime;
 
-        if(disappearTickCooldown <= 0f)
-            Destroy(Object);
+        if(currentTick >= disappearTickCooldown)
+            OnDisappearedEvent.Invoke(Object);
     }
 
-    private void TakeOrDrop() => isTaken = !isTaken;
+    public void TakeOrDrop() => isTaken = !isTaken;
+
+    public void OnDisappeared(GameObject Object) => Destroy(Object);
 }
